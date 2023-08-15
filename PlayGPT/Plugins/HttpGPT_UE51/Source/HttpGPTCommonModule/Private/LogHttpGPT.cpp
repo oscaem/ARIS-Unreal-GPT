f2c19b3cfda889717c7005549ab354d60b2ab5ef
oscaem <oemp@icloud.com>
2023-08-15 19:01:30 +0200

    published initial source code

--- PlayGPT/Plugins/HttpGPT_UE51/Source/HttpGPTCommonModule/Private/LogHttpGPT.cpp
+++
@@ -1,8 +0,0 @@
-// Author: Lucas Vilas-Boas
-// Year: 2023
-// Repo: https://github.com/lucoiso/UEHttpGPT
-
-#include "LogHttpGPT.h"
-
-DEFINE_LOG_CATEGORY(LogHttpGPT);
-DEFINE_LOG_CATEGORY(LogHttpGPT_Internal);
